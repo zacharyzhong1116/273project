a4014de92bae2161426144dc8a937e31a5c72ab0
Wenjin Ma <xiaoshan1213@gmail.com>
2017-04-28 16:33:58 -0700

    Rename client

---
+++ FaceRecognition_AndroidClient/app/src/main/java/com/cmpe273/sam/facerecognition/MainActivity.java
@@ -0,0 +1,176 @@
+package com.cmpe273.sam.facerecognition;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 100;
+    private static final String IMAGE_LOCATION = "cmpe273 project";
+    private Uri fileUri;
+    private Uri selectedImage;
+    private String imgPath;
+    private String ba1;
+    ImageView mImageView;
+    Button btnJumpToCam;
+    Button btnUpload;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageView = (ImageView) findViewById(R.id.imgView);
+        btnJumpToCam = (Button) findViewById(R.id.btnJump);
+        btnUpload = (Button) findViewById(R.id.btnUpload);
+
+        btnJumpToCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jumpToCamera();
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                upload();
+            }
+        });
+    }
+
+    public void jumpToCamera(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //fileUri = Uri.fromFile(getOutPutImageFile());
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+        else {
+            Toast.makeText(getApplication(), "Camera not supported", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            selectedImage = data.getData();
+            Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+            mImageView.setImageBitmap(imageBitmap);
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            imgPath = cursor.getString(columnIndex);
+            cursor.close();
+        }
+    }
+
+    private static File getOutPutImageFile(){
+        File imageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),IMAGE_LOCATION);
+        if(!imageDir.exists()){
+            if(!imageDir.mkdirs()){
+                Log.d(IMAGE_LOCATION, "failed to create image directory");
+                return null;
+            }
+        }
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        mediaFile = new File(imageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+        return mediaFile;
+    }
+
+    public void upload() {
+       // Bitmap bm = BitmapFactory.decodeFile(imgPath);
+        Bitmap bm= ((BitmapDrawable) mImageView.getDrawable()).getBitmap();
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 50, bao);
+
+
+        byte[] ba = bao.toByteArray();
+        ba1 = Base64.encodeToString(ba, Base64.NO_WRAP);
+
+        //create upload worker and execute
+        RecognitionWorker worker = new RecognitionWorker();
+        worker.execute();
+    }
+
+    /**
+     * inner upload conn class
+     */
+    public class RecognitionWorker extends AsyncTask<Void, Void, String> {
+
+        private ProgressDialog pd;
+
+        public RecognitionWorker () {
+            pd = new ProgressDialog(MainActivity.this);
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try{
+                URL url = new URL("http://localhost");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                //json or ?
+                ContentValues cv = new ContentValues();
+                cv.put("data", ba1);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(cv.toString());
+                writer.flush();
+                writer.close();
+                os.close();
+
+                conn.connect();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return "Success";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            pd.hide();
+            pd.dismiss();
+        }
+    }
+}
